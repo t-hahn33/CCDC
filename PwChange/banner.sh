8b6df3ce5b80f072a5022da04d7d0016d1017c57
t-hahn33 <153419975+t-hahn33@users.noreply.github.com>
2024-01-24 23:27:38 -0600

    banner.sh

--- PwChange/banner.sh
+++ PwChange/banner.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "**********************************************************************"
-echo "**                    RESTRICTED ACCESS AREA                       **"
-echo "**********************************************************************"
-echo
+
+echo                      "RESTRICTED ACCESS AREA"                       
+
+echo "WARNING: This is a secure system. Unauthorized access is prohibited." 
+
 echo "This system is for authorized personnel only. Unauthorized access"
 echo "or use is strictly prohibited. Violators will be prosecuted to the"
 echo "fullest extent of the law."
@@ -15,6 +16,7 @@ echo "- Access to this system implies consent to monitoring."
 echo
 echo "Unauthorized access is a violation of company policy and may result"
 echo "in disciplinary action, civil liability, and criminal prosecution."
-echo
-echo "** WARNING: This is a secure system. Unauthorized access is prohibited. **"
-echo "**********************************************************************"
+
+echo  "WARNING: This is a secure system. Unauthorized access is prohibited." 
+
+echo                      "RESTRICTED ACCESS AREA"
