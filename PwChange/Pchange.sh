78ac5c0c1718d1615b55b0cb45385f4a22902c4b
t-hahn33 <153419975+t-hahn33@users.noreply.github.com>
2024-01-24 20:31:12 -0600

    Update Pchange.sh

--- PwChange/Pchange.sh
+++ PwChange/Pchange.sh
@@ -16,4 +16,11 @@ for username in $(awk -F':' '{ if ($3 >= 1000 && $1 != "nobody") print $1 }' /et
   echo -e "$new_password\n$new_password" | passwd --stdin $username
 done
 
+# Change MySQL passwords for all users
+mysql_users=$(mysql -Bse "SELECT User FROM mysql.user WHERE User != 'root';")
+for mysql_user in $mysql_users; do
+  echo "Changing MySQL password for user: $mysql_user"
+  mysqladmin -u $mysql_user password "$new_password"
+done
+
 echo "Password change script completed."
