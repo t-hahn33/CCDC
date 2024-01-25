68737b598b0677aef390284f9af971be86cdd7c0
t-hahn33 <153419975+t-hahn33@users.noreply.github.com>
2024-01-24 23:33:36 -0600

    banner.sh

--- PwChange/banner.sh
+++ PwChange/banner.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-echo"******************************************************************************"
+echo "******************************************************************************"
 echo                      "RESTRICTED ACCESS AREA"                       
-echo"******************************************************************************"
-echo "WARNING: This is a secure system. Unauthorized access is prohibited." 
+echo "******************************************************************************"
+echo  "WARNING: This is a secure system. Unauthorized access is prohibited." 
 
-echo "This system is for authorized personnel only. Unauthorized access"
-echo "or use is strictly prohibited. Violators will be prosecuted to the"
-echo "fullest extent of the law."
-echo
-echo "Security Reminder:"
-echo "- Ensure your login credentials are kept confidential."
-echo "- Report any suspicious activity immediately."
-echo "- Access to this system implies consent to monitoring."
+echo  "This system is for authorized personnel only. Unauthorized access"
+echo  "or use is strictly prohibited. Violators will be prosecuted to the"
+echo  "fullest extent of the law."
 
-echo "Unauthorized access is a violation of company policy and may result"
-echo "in disciplinary action, civil liability, and criminal prosecution."
+echo  "Security Reminder:"
+echo  "- Ensure your login credentials are kept confidential."
+echo  "- Report any suspicious activity immediately."
+echo  "- Access to this system implies consent to monitoring."
+
+echo  "Unauthorized access is a violation of company policy and may result"
+echo  "in disciplinary action, civil liability, and criminal prosecution."
 
 echo  "WARNING: This is a secure system. Unauthorized access is prohibited." 
-echo"*****************************************************************************"
+echo "*****************************************************************************"
 echo                      "RESTRICTED ACCESS AREA"
-echo"*****************************************************************************"
+echo "*****************************************************************************"
